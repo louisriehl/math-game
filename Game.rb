248c687f9a4f6fe37fafc5618bd8c753650a9118
Louis Riehl <louisriehl@gmail.com>
2018-11-05 20:38:47 +0000

    pass question parameters to game

--- Game.rb
+++ Game.rb
@@ -10,7 +10,13 @@ class Game
   end
 
   def run_game
+    puts "#{@players[0].name}: #{get_question}"
+  end
 
+  def get_question
+    question = Question.new
+    question_output = question.get_question_array
+    "What is #{question_output[0]} + #{question_output[1]}? \n Pssst, it's #{question_output[2]}"
   end
 
 end
--- main.rb
+++ main.rb
@@ -3,4 +3,7 @@
 require './Game.rb'
 require './Player.rb'
 require './Question.rb'
-require './Turn.rb'
+
+the_game = Game.new
+
+the_game.run_game
