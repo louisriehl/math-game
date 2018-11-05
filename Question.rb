3a33d6667fd6150e6d4ac0eddb7ce2c3ea97d83d
Louis Riehl <louisriehl@gmail.com>
2018-11-05 20:42:22 +0000

    store answers as state

--- Question.rb
+++ Question.rb
@@ -4,19 +4,16 @@ class Question
 
   def initialize
     @random_int = Random.new
+    @integers = self.get_random_numbers
+    @answer = self.get_correct_answer(@integers)
   end
 
   def get_random_numbers
-    [@random_int.rand(MAXIMUM_INTEGER), @random_int.rand(MAXIMUM_INTEGER)]
+    @integers = [@random_int.rand(MAXIMUM_INTEGER), @random_int.rand(MAXIMUM_INTEGER)]
   end
 
   def get_correct_answer(array)
     array[0] + array[1]
-  end
-
-  def get_question_array
-    question_array = self.get_random_numbers
-    question_array << self.get_correct_answer(question_array)
   end
 
 end
